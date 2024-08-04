5382a08604f960bba2a0bc3fd122413482ac1b5e
parvinsorath1 <pguj6399@gmail.com>
2024-08-04 21:53:05 +0530

    added index.htm added script.js add style.css

---
+++ script.js
@@ -0,0 +1,71 @@
+let highestZ = 1;
+
+class Paper{
+
+    holdingPaper = false;
+
+    prevMouseX = 0;
+    prevMouseY = 0;
+
+    mouseX = 0;
+    mouseY = 0;
+
+    veloictyX = 0;
+    veloictyY = 0;
+
+    currentPaperX = 0;
+    currentPaperY = 0;
+
+    init(paper) {
+
+        paper.addEventListener('mousedown', (e) => {
+            
+            this.holdingPaper = true;
+
+            paper.style.zIndex = highestZ;
+            highestZ+= 1;
+
+            if( e.button === 0) {
+                this.prevMouseX = this.mouseX;
+                this.prevMouseY = this.mouseY
+
+                console.log (this.prevMouseX);
+                console.log (this.prevMouseY);
+            }
+
+        })
+
+        document.addEventListener('mousemove',(e) => {
+            
+            this.mouseX = e.clientX;
+            this.mouseY = e.clientY;
+
+            this.veloictyX = this.mouseX - this.prevMouseX;
+            this.veloictyY = this.mouseY - this.prevMouseY;
+
+            if(this.holdingPaper) {
+
+                this.currentPaperX += this.veloictyX;
+                this.currentPaperY += this.veloictyY;
+
+                this.prevMouseX = this.mouseX;
+                this.prevMouseY = this.mouseY;
+
+                paper.style.transform = `translateX(${this.currentPaperX}px) translateY(${this.currentPaperY}px)`;
+            }
+        })
+
+        window.addEventListener('mouseup' , (e) => {
+            console.log('mouse button is released');
+            this.holdingPaper = false;
+        })
+    }
+}
+
+const papers=Array.from(document.querySelectorAll('.paper'));
+   
+papers.forEach( paper => {
+    const p = new Paper();
+    p.init(paper);
+})
+
